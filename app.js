6a8754a855c62c3c9693c2e532884e5ff60bda85
kevincoleman <kevin.n.coleman@gmail.com>
2015-04-20 16:12:42 -0700

    auto-add screenshots to git repo

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 var chokidar = require('chokidar');
+var nodegit = require('nodegit');
+var path = require('path');
 
 var watcher = chokidar.watch('shots/', {ignored: /^\./, persistent: true});
 
+var repo;
+var index;
+var oid;
+var parent;
+
 watcher
-  .on('add', function(path) {console.log('File', path, 'has been added');})
-  .on('change', function(path) {console.log('File', path, 'has been changed');})
-  .on('unlink', function(path) {console.log('File', path, 'has been removed');})
+  .on('add', function(screenShot) {
+
+    // commit newest file
+    nodegit.Repository.open(path.resolve(__dirname, './.git'))
+    .then(function(repo) {
+      return repo.openIndex();
+    })
+    .then(function(indexResult) {
+      index = indexResult;
+      return index.read(1);
+    })
+    .then(function() {
+      return index.addByPath(screenShot);
+    })
+    .then(function() {
+      return index.write();
+    })
+    .done(function() {
+      console.log('File', screenShot, 'has been added');
+    });
+
+    })
+  .on('change', function(screenShot) {console.log('File', screenShot, 'has been changed');})
+  .on('unlink', function(screenShot) {console.log('File', screenShot, 'has been removed');})
   .on('error', function(error) {console.error('Error happened', error);});
