3032d6bd9848233bbc9252583aac09aa36a0e1d7
kevincoleman <kevin.n.coleman@gmail.com>
2015-04-22 15:54:13 -0700

    add validation to keep temp write files out of staging

--- app.js
+++ app.js
@@ -12,24 +12,28 @@ var parent;
 watcher
   .on('add', function(screenShot) {
 
-    // commit newest file
-    nodegit.Repository.open(path.resolve(__dirname, './.git'))
-    .then(function(repo) {
-      return repo.openIndex();
-    })
-    .then(function(indexResult) {
-      index = indexResult;
-      return index.read(1);
-    })
-    .then(function() {
-      return index.addByPath(screenShot);
-    })
-    .then(function() {
-      return index.write();
-    })
-    .done(function() {
-      console.log('File', screenShot, 'has been added');
-    });
+    if (screenShot.indexOf(".Screen") == -1) {
+      console.log(screenShot);
+      // commit newest file
+      nodegit.Repository.open(path.resolve(__dirname, './.git'))
+      .then(function(repo) {
+        return repo.openIndex();
+      })
+      .then(function(indexResult) {
+        index = indexResult;
+        return index.read(1);
+      })
+      .then(function() {
+        return index.addByPath(screenShot);
+      })
+      .then(function() {
+        return index.write();
+      })
+      .done(function() {
+        console.log('File', screenShot, 'has been added');
+      });
+
+    }
 
     })
   .on('change', function(screenShot) {console.log('File', screenShot, 'has been changed');})
